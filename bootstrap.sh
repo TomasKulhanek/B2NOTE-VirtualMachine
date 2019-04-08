e5b79db4b2ad3500f800a8920ec5518a369b6458
Tomas Kulhanek <tomaton@centrum.cz>
2019-04-08 09:48:31 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -197,6 +197,8 @@ service httpd restart
 
 # install nodejs v >8.x required by aurelia
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+# remove previous nodejs installation
+yum -y remove nodejs
 yum -y install nodejs
 npm install aurelia-cli -g --quiet
 
