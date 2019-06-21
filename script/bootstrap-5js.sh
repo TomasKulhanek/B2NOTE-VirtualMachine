b446d5feff1b90cec407a31df3757515d55fc353
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-06-21 16:49:49 +0200

    add unit tests at the end

--- script/bootstrap-5js.sh
+++ script/bootstrap-5js.sh
@@ -4,3 +4,15 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 yum -y remove nodejs
 yum -y install nodejs
 npm install aurelia-cli -g --quiet
+
+echo Testing B2Note, if all tests PASSED, then B2NOTE is installed correctly
+cd /home/vagrant/b2note/b2note_api
+python -m unittest
+echo Frontend APP UI unit tests
+cd ..
+cd b2note_app
+au test
+echo Frontend APP UI e2e tests
+au run & sleep 10; au teste2e
+killall au
+
